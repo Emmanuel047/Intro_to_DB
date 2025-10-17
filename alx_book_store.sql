e7cc5ffc679d225311d15ca1d96717219b6f453d
Emmanuel047 <emwenwa007@gmail.com>
2025-10-17 11:45:35 +0300

    chage

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,9 +3,7 @@ USE alx_book_store;
 
 CREATE TABLE IF NOT EXISTS Authors(
 	author_id INT PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
-    address TEXT
+    author_name VARCHAR(215)
 );
 
 CREATE TABLE IF NOT EXISTS Books(
@@ -28,16 +26,15 @@ CREATE TABLE IF NOT EXISTS Customers(
 CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-    order_date DATE
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY  (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
-    
