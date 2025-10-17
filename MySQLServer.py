4d54e1615f953b7d681f9bcc7d620f65b6095b81
Emmanuel047 <emwenwa007@gmail.com>
2025-10-17 12:36:40 +0300

    error

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,15 +1,18 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-mydb = mysql.connector.connect(
+try:
+    mydb = mysql.connector.connect(
     host = "127.0.0.1",
     user = "root",
     password = "root"
-)
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
 
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database 'alx_book_store' created successfully!")
-
-mycursor.close()
-mydb.close()
+except mysql.connector.Error as e:
+        print(f"An error occured {e}")
+else:
+    mycursor.close()
+    mydb.close()
